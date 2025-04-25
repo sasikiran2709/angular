ee86de3c36368ae066b8b1496ccaa96756e0b472
sasikiran2709 <sasikiranpamuru9@gmail.com>
2025-04-25 11:40:15 +0530

    some chages made

--- angular/src/app/b/b.component.ts
+++ angular/src/app/b/b.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { AComponent } from "../a/a.component";
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+
 
 
 @Component({
   selector: 'app-b',
   standalone:true,
-  imports: [AComponent,FormsModule],
+  imports: [AComponent,CommonModule,FormsModule],
   templateUrl: './b.component.html',
   styleUrl: './b.component.css'
 })
@@ -16,7 +19,7 @@ export class BComponent {
   y:number=10;
   z:number=10;
   L:number=this.x+this.y+this.z;
-  per_name:string="";
+  per_name:string='';
 save(){
 
 }
